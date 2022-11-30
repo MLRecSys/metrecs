8abca19fcd74c8279dfd6775089b185a2c1f10f9
jkrse <johannes-kruse@hotmail.com>
2022-11-30 15:58:44 +0100

    WIP add branch

--- src/metrecs/metrics/fragmentation.py
+++ src/metrecs/metrics/fragmentation.py
@@ -1,9 +1,10 @@
 import numpy as np
 from numpy.typing import ArrayLike
-from typing import Any
+from typing import Any, Dict
 from metrecs.utils import histogram
 from metrecs.utils import compute_kl_js_divergence
-
+from scipy.spatial import distance
+from collections import Counter
 
 def fragmentation(
     ref: ArrayLike[ArrayLike[Any]], own: ArrayLike[Any], catelog: ArrayLike[Any]
@@ -12,11 +13,11 @@ def fragmentation(
     Calculates to what extent users have been exposed to the same "item clusters".
     An "item cluster" is considered a set of items that are related.
 
-    We measure Fragmentation as the average of JS divergence between every pair of users' recommendations.
+    We measure Fragmentation as the average of Jensen-Shannon (JS) divergence between every pair of users' recommendations.
     Fragmentation tells us whether users exist in their own bubble of items or are part of a greater public sphere.
 
-    For KL and JS divergence, P(ref[u]) is the rank-aware distribution of "item clusters" for a reference user u,
-    and Q(own) the same but for the current user.
+    For Kullback-Leibler (KL) and JS divergence, P(ref[u]) is the rank-aware distribution of "item clusters"
+    for a reference user u, and Q(own) the same but for the current user.
 
     Args:
         ref: Two-dimensional array of ordered "item cluster" ids from the other users
@@ -31,3 +32,58 @@ def fragmentation(
     ref_hist = np.apply_along_axis(histogram, 1, ref)
     _, jsd = np.mean([compute_kl_js_divergence(own_hist, p) for p in ref_hist])
     return jsd
+
+
+# the difference in distribution between the issued recommendations (Q) and its context (P). Eac
+# The context P may refer to either the overall supply of available items,
+# the user profile, such as the reading history or explicitly stated preferences, or the recommendations that were issued to other users
+
+
+def fragmentation(
+    recommendations: ArrayLike[ArrayLike[str]], context: ArrayLike[str] = None
+) -> float:
+    """
+    Calculates to what extent users have been exposed to the same "item clusters".
+    An "item cluster" is considered a set of items that are related.
+
+    We measure Fragmentation as the average of JS divergence between every pair of users' recommendations.
+    Fragmentation tells us whether users exist in their own bubble of items or are part of a greater public sphere.
+
+    For KL and JS divergence, P(ref[u]) is the rank-aware distribution of "item clusters" for a reference user u,
+    and Q(own) the same but for the current user.
+
+    Args:
+        recommendations: Two-dimensional array of ordered "item cluster" ids from the other users
+
+    Returns:
+        float: fragmentation score
+    """
+    # TODO ASSERT: len(user_item_representaion) == other_users_item_representaion.shape[1]
+    if not context:
+        context = recommendations
+
+    recommendation_distribution = np.apply_along_axis(histogram, 1, recommendations)
+    context_distribution = np.apply_along_axis(histogram, 0, context)
+
+    for index, recommendation in enumerate(recommendations):
+
+        user_distribution = histogram(recommendations, rank_aware=True)
+        context = np.delete(ref_hist, index, axis=0)
+
+        jsd = np.mean([distance.jensenshannon(user_distribution, p) for p in ref_hist])
+        print(jsd)
+    return jsd
+
+
+
+Counter(recommendations[0])
+
+n = np.unique(recommendations[0], return_counts=True)[1]
+
+own_hist = histogram(recommendations[0], rank_aware=False)
+ref_hist = np.apply_along_axis(histogram, 1, recommendations)
+
+jsd = [distance.jensenshannon(own_hist, p) for p in ref_hist]
+
+
+distance.jensenshannon(own_hist, ref_hist)
--- src/metrecs/metrics/intralist_diversity.py
+++ src/metrecs/metrics/intralist_diversity.py
@@ -5,25 +5,26 @@ import numpy as np
 
 
 def intralist_diversity(
-    items: ArrayLike[ArrayLike], distance_function: Callable = cosine_distances
+    recommendations: ArrayLike[ArrayLike],
+    distance_function: Callable = cosine_distances,
 ) -> float:
     """Smyth and McClave [2001]
     * Source: https://link.springer.com/chapter/10.1007/3-540-44593-5_25
-    * Measure the intra-list diversity of a recommendation list R(|R|>1) as the average pairwise distance between items:
+    * Measure the intra-list diversity of a recommendation list R(|R|>1) as the average pairwise distance between recommendations:
         Diversity(R) = ( sum_{i∈R} sum_{j∈R\{i}} dist(i, j) )  / ( |R|(|R|-1) )
 
     Args:
-        items (np.ndarray): {array-like, sparse matrix} of shape (n_samples_X, n_features)
-        distance_function(X): function to compute the pairwise distances of items vector representations
+        recommendations (ArrayLike[ArrayLike]): {array-like, sparse matrix} of shape (n_samples_X, n_features)
+        distance_function (Callable): function to compute the pairwise distances of recommendations vector representations. Default is cosine_distances
 
     Returns:
         float: diversity score
     """
-    if len(items.shape) == 1:
-        # Less than or equal to 1 items in recommendation list
+    if len(recommendations.shape) == 1:
+        # Less than or equal to 1 recommendations in recommendation list
         diversity = np.nan
     else:
-        R = items.shape[0]
-        pairwise_distances = distance_function(items)
+        R = recommendations.shape[0]
+        pairwise_distances = distance_function(recommendations)
         diversity = np.sum(pairwise_distances) / (R * (R - 1))
     return diversity
