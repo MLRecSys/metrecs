6ca1bf71b66ee0b5dc796b21c6038f8ad183b150
jkrse <johannes-kruse@hotmail.com>
2022-12-13 18:12:52 +0100

    documentation

--- src/metrecs/utils.py
+++ src/metrecs/utils.py
@@ -18,7 +18,7 @@ def harmonic_number(n: int) -> float:
     return gamma + math.log(n) + 0.5 / n - 1.0 / (12 * n**2) + 1.0 / (120 * n**4)
 
 
-def normalized_scaled_harmonic_number_series(n: int) -> np.ndarray[float]:
+def normalized_scaled_harmonic_number_series(n: int) -> ArrayLike[float]:
     """Return an array of scaled normalized harmonic numbers
 
     Args:
@@ -49,54 +49,67 @@ def cosine_distances(X: ArrayLike) -> np.ndarray:
 
 def compute_normalized_distribution(
     a: np.ndarray[str],
-    weights: np.ndarray[float] = [],
-    distribution: Dict[str, float] = {},
-) -> Dict:
-    """_summary_
+    weights: np.ndarray[float] = None,
+    distribution: Dict[str, float] = None,
+) -> Dict[str, float]:
+    """
+    Compute a normalized weigted distribution for a list of items that each can have a single representation assigned.
+
     Args:
-        a (np.ndarray[str]): _description_
-        weights (np.ndarray[float], optional): _description_. Defaults to [].
-        distribution (Dict[str, float], optional): _description_. Defaults to {}.
+        a (np.ndarray[str]): a list/array of items representation.
+        weights (ArrayLike[float], optional): weights to assign each element in a. Defaults to None.
+            * Following yields: len(weights) == len(a)
+        distribution (Dict[str, float], optional): dictionary to assign the distribution values, if None it will be generated as {}. Defaults to None.
+            * Use case; if you want to add distribution values to existing, one can input it.
+
     Returns:
-        Dict: _description_
+        Dict[str, float]: dictionary with normalized distribution values
+
     >>> a = np.array(["a", "b", "c", "c"])
     >>> weights = np.array([1 / rank / harmonic_number(len(a)) for rank in range(1, len(a) + 1)])
     >>> compute_distribution(a, weights=weights)
         {'a': 0.4799997900047559, 'b': 0.23999989500237795, 'c': 0.2799998775027743}
+    >>> compute_distribution(a)
+        {'a': 0.25, 'b': 0.25, 'c': 0.5}
     """
-    n_values = len(a)
+
+    n_elements = len(a)
 
     distr = {} if not distribution else distribution
-    weights = weights if np.any(weights) else np.ones(n_values) / n_values
+    weights = weights if weights else np.ones(n_elements) / n_elements
     for item, weight in zip(a, weights):
         distr[item] = weight + distr.get(item, 0.0)
     return distr
 
 
-# TODO: write clean make tests
 def compute_normalized_distribution_multiple_categories(
     a: List[set[str]],
-    weights: np.ndarray[float] = [],
-    distribution: Dict[str, float] = {},
-) -> Dict:
-    """_summary_
+    weights: np.ndarray[float] = None,
+    distribution: Dict[str, float] = None,
+) -> Dict[str, float]:
+    """
+    Compute a normalized weigted distribution for a list of items that each can have multiple representation assigned.
+    For instance, a list of news articles that each can have multiple categoies, e.g. 'politics' + 'economy' OR 'entertainment' + 'sport'.
+
     Args:
-        a (np.ndarray[str]): list of list of a [LET PEOPLE KNOW THAT IS IS A SET OF UNIQUE ITEMS PER LIST]
-        weights (np.ndarray[float], optional): _description_. Defaults to [].
-        distribution (Dict[str, float], optional): _description_. Defaults to {}.
+        a (List[set[str]]): a list of sets of items representation.
+        weights (ArrayLike[float], optional): weights to assign each set in the list. Defaults to None.
+            * Following yields: len(weights) == len(a)
+        distribution (Dict[str, float], optional): dictionary to assign the distribution values, if None it will be generated as {}. Defaults to None.
+            * Use case; if you want to add distribution values to existing, one can input it.
     Returns:
-        Dict: _description_
-    >>> a = [set(['a', 'x']), set(['b', 'c', 'x']), set(['c', 'a', 'y', 'x', 'q', 't']), set(['c', 'b'])]
+        Dict[str, float]: dictionary with normalized distribution values
+    >>> a = [['a', 'x'], ['b', 'c', 'x'], ['c', 'a', 'y', 'x', 'q', 't'], ['c', 'b']]
     >>> weights = np.array([1 / rank / harmonic_number(len(a)) for rank in range(1, len(a) + 1)])
     >>> compute_distribution_multiple_categories(a, weights=weights)
     """
     n_elements = len(a)
 
     distr = {} if not distribution else distribution
-    weights = weights if np.any(weights) else np.ones(n_elements) / n_elements
+    weights = weights if not weights else np.ones(n_elements) / n_elements
     for item, weight in zip(a, weights):
         for cat in set(item):
-            distr[cat] = weight + distr.get(cat, 0.0)  # use this
+            distr[cat] = weight + distr.get(cat, 0.0)
     norm_ = sum(distr.values())
     return {key: val / norm_ for key, val in distr.items()}
 
